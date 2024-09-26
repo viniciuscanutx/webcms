e94c7b9d0e1d500e583a6c698ee66f97e5ac3c1a
viniciusucanutx <viniciuscanutmz@gmail.com>
2024-09-26 16:54:25 -0300

    :construction: Fomulary for add movies and series

---
+++ src/app/components/user/FormUsers.tsx
@@ -0,0 +1,41 @@
+import { Users } from "@/core/model/Users"
+import InputText from "../shared/InputText"
+import TextArea from "../shared/TextArea"
+
+export interface FormUsersProps {
+    usuario: Users
+    onChange: (usuario: Users) => void
+    onSave: () => void
+    onCancel: () => void
+}
+
+export default function FormUsers(props: FormUsersProps) {
+    return (
+        <div className="flex flex-col gap-5">
+            <InputText className="p-3"
+            label="Titulo" 
+            type="text" 
+            value={props.usuario.nome} 
+            onChange={
+                (e) => props.onChange?.({ ...props.usuario, nome: e.target.value })} 
+            />
+            <InputText className="p-3"
+            label="Poster" 
+            type="text" 
+            value={props.usuario.poster} 
+            onChange={
+                (e) => props.onChange?.({ ...props.usuario, poster: e.target.value })} 
+            />
+            <TextArea className="p-3 bg-zinc-800 w-full h-auto resize-none overflow-y-auto break-words rounded-md"
+            label="Overview"
+            value={props.usuario.email} 
+            onChange={
+                (e) => props.onChange?.({ ...props.usuario, email: e.target.value })} 
+            />
+           <div className="flex gap-5">
+                <button className="bg-blue-500 px-4 py-2 rounded-md" onClick={props.onSave}>Salvar</button>
+                <button className="bg-zinc-500 px-4 py-2 rounded-md" onClick={props.onCancel}>Cancelar</button>
+           </div>
+        </div>
+    )
+}
