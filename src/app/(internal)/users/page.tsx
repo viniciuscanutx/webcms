4324a72480edab5878cf85895ca3589caf518fed
viniciusucanutx <viniciuscanutmz@gmail.com>
2024-09-26 16:54:50 -0300

    :test_tube: test: Testing formulary

--- src/app/(internal)/users/page.tsx
+++ src/app/(internal)/users/page.tsx
@@ -1,13 +1,20 @@
+'use client';
 import Pagina from "@/app/components/template/Page";
 import Title from "@/app/components/template/Title";
 import UserList from "@/app/components/user/UserList";
+import FormUsers from "@/app/components/user/FormUsers";
+import User from "@/app/data/constants/users";
 import { IconMovie } from "@tabler/icons-react";
+import { useState } from "react";
+import { Users } from "@/core/model/Users";
 
 export default function Page() {
+    const [usuario, setUsuario] = useState<Users>(User[0])
     return (
         <Pagina className="flex flex-col gap-20">
             <Title icone={IconMovie} principal="Filmes" secundario="Cadastro de Filmes"/>
-            <UserList />
+            {/* <UserList /> */}
+            <FormUsers usuario={usuario} onChange={setUsuario} />
         </Pagina>
     )
 }
--- src/app/components/template/Page.tsx
+++ src/app/components/template/Page.tsx
@@ -3,13 +3,14 @@ import Menu from "./Menu"
 /* Fazendo o elemento pagina aceitar elementos filhos. */
 export interface PaginaProps {
     children: any
+    className?: string
 }
 
 export default function Pagina(props: PaginaProps) {
     return (
         <div className="flex">
             <Menu />
-            <main className="flex-1 p-7">{props.children}</main>
+            <main className={`flex-1 p-7 ${props.className ?? ''}`}>{props.children}</main>
             </div>
     )
 }
