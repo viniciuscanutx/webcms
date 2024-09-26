658974fed5868c66cc4745655cae9d0431461266
viniciusucanutx <viniciuscanutmz@gmail.com>
2024-09-26 12:26:41 -0300

    :bulb: docs: Creating fake users

---
+++ src/app/data/constants/usuarios.tsx
@@ -0,0 +1,28 @@
+import { Usuario } from "@/core/model/Usuario";
+
+const usuarios: Usuario[] = [
+    {
+        id: "1",
+        email: "jose@empresa.com.br",
+        nome: "Ana",
+        senha: "12345"
+    },
+    {
+        id: "2",
+        email: "pedro@empresa.com.br",
+        nome: "Pedro",
+        senha: "12345"
+    },
+    {
+        id: "3",
+        email: "elias@empresa.com.br",
+        nome: "Elias",
+        senha: "12345"
+    },
+    {
+        id: "4",
+        email: "juliana@empresa.com.br",
+        nome: "Juliana",
+        senha: "12345"
+    },
+]
